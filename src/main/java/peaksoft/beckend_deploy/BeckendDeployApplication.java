c71e3ac86408fdef5abb97f22ea21ad7bcd1102b
admin <muhammedbatyrov1015@gmail.com>
2023-03-01 18:21:37 +0600

    Initial commit

---
+++ src/main/java/peaksoft/beckend_deploy/BeckendDeployApplication.java
@@ -0,0 +1,13 @@
+package peaksoft.beckend_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeckendDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeckendDeployApplication.class, args);
+    }
+
+}
